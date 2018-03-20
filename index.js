50e846cdbae48afcc829bce3b32eac91897afb29
Ben Sharafian <benms64@gmail.com>
2018-03-20 16:57:03 -0700

    fix: path

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const Koa = require('koa')
 const router = require('koa-router')()
-const serve = require('koa-static')('./static')
+const path = require('path')
+const serve = require('koa-static')(path.resolve(__dirname, 'static'))
 const app = new Koa()
 
 const KoaWebMonetization = require('koa-web-monetization')
 const monetization = new KoaWebMonetization({ maxBalance: 25 })
 
 const fs = require('fs-extra')
-const path = require('path')
 
 router.get('/client.js', async ctx => {
   ctx.body = await fs.readFile(path.resolve(__dirname, 'node_modules/koa-web-monetization/client.js'))
